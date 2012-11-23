cd0995c3fdc92dba1f63d2a1aa1b3de10eaeccb1
Longkai Cao <lkcao@thoughtworks.com>
2012-11-23 15:25:47 +0800

    commandline

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    static private String helloString = "Hello World!";
+
+    public static void main(String args[]) {
+	System.out.println(helloString);
+    }
+
+    public String getString() {
+	return helloString;
+    }
+}
