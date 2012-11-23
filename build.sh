0b74271dfb67255d01af7e52e79b578c60712ad6
Longkai Cao <lkcao@thoughtworks.com>
2012-11-23 16:39:26 +0800

    rm build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "compiling source code..."
-javac HelloWorld.java 
-echo "compiling test code..."
-javac -classpath 'junit-4.10.jar:./' HelloWorldTest.java
-echo "testing..."
-java -classpath 'junit-4.10.jar:./' org.junit.runner.JUnitCore HelloWorldTest
