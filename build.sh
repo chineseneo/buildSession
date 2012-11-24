01775f0eee91e4098aa917b46885965a12b46471
Longkai Cao <lkcao@thoughtworks.com>
2012-11-24 11:52:39 +0800

    add clean and package job to build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+./clean.sh
+
 echo "compiling source code..."
 javac HelloWorld.java 
 echo "compiling test code..."
 javac -classpath 'junit-4.10.jar:./' HelloWorldTest.java
 echo "testing..."
 java -classpath 'junit-4.10.jar:./' org.junit.runner.JUnitCore HelloWorldTest
+echo "packaging..."
+jar cfm build.jar manifest.txt HelloWorld.class 
+echo "All done!"
---
+++ clean.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "cleaning working directory..."
+rm -rf *.class
+rm -rf build.jar
+echo "done!"
